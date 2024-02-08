5febd06b083521ba20fb322e62e819368e7742ca
Alexander <axlisovsky@gmail.com>
2024-02-09 00:42:32 +0200

    segment loading

---
+++ src/app/(protected)/loading.tsx
@@ -0,0 +1,19 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card'
+import { Skeleton } from '@/components/ui/skeleton'
+
+export default function Loading() {
+  return (
+    <Card className='w-[600px] shadow-md'>
+      <CardHeader>
+        <Skeleton className='mx-auto w-[300px] h-[32px] rounded-lg' />
+      </CardHeader>
+      <CardContent className='space-y-4'>
+        <Skeleton className='w-full h-[50px] rounded-lg' />
+        <Skeleton className='w-full h-[50px] rounded-lg' />
+        <Skeleton className='w-full h-[50px] rounded-lg' />
+        <Skeleton className='w-full h-[50px] rounded-lg' />
+        <Skeleton className='w-full h-[50px] rounded-lg' />
+      </CardContent>
+    </Card>
+  )
+}
--- src/app/(protected)/server/page.tsx
+++ src/app/(protected)/server/page.tsx
@@ -1,7 +1,14 @@
 import { currentUser } from '@/lib/auth'
 import { UserInfo } from '@/components/user-info'
 
+async function getTickets() {
+  // imitate delay
+  await new Promise((resolve) => setTimeout(resolve, 3000))
+  return false
+}
+
 const ServerPage = async () => {
+  const tickets = await getTickets()
   const user = await currentUser()
 
   return <UserInfo label='💻 Server component' user={user} />
